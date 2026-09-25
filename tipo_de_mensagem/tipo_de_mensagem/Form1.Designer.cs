09271b12b1bdedcc127d0da47fc804c04ba9888c
soniocpistoleiro <suamae2627@gmail.com>
2026-09-24 21:49:51 -0300

    update

---
+++ tipo_de_mensagem/tipo_de_mensagem/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace tipo_de_mensagem
+{
+    partial class frm_tiposdemensagens
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tipos de Mensagens";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // frm_tiposdemensagens
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::tipo_de_mensagem.Properties.Resources.bloodied_kurosaki_ichigo_manga_hcwxz4y5lhrtm6sr__2___1___1_;
+            this.ClientSize = new System.Drawing.Size(1004, 535);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximumSize = new System.Drawing.Size(1020, 574);
+            this.MinimumSize = new System.Drawing.Size(920, 504);
+            this.Name = "frm_tiposdemensagens";
+            this.Text = "TIPO DE MENSAGENS";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
